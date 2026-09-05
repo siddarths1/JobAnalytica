1c0a5849a5788ade98386bbc0fd500e9bad8a6f0
SIDDARTH S <91017970+siddarths1@users.noreply.github.com>
2026-09-05 21:58:23 +0530

    feat(discovery): add VisualDiscoveryController and DiscoveryModule registration

--- apps/api/src/modules/discovery/discovery.module.ts
+++ apps/api/src/modules/discovery/discovery.module.ts
@@ -1,14 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DiscoveryController } from './discovery.controller';
 import { DiscoveryService } from './discovery.service';
 import { AtsResolverService } from './ats-resolver.service';
 import { FreshnessValidatorService } from './freshness-validator.service';
+import { VisualDiscoveryService } from './visual-discovery.service';
+import { VisualDiscoveryController } from './visual-discovery.controller';
 import { JobsModule } from '../jobs/jobs.module';
+import { ApplicationsModule } from '../applications/applications.module';
 
 @Module({
-  imports: [JobsModule],
-  controllers: [DiscoveryController],
-  providers: [DiscoveryService, AtsResolverService, FreshnessValidatorService],
-  exports: [DiscoveryService, AtsResolverService, FreshnessValidatorService],
+  imports: [
+    forwardRef(() => JobsModule),
+    forwardRef(() => ApplicationsModule),
+  ],
+  controllers: [DiscoveryController, VisualDiscoveryController],
+  providers: [
+    DiscoveryService,
+    AtsResolverService,
+    FreshnessValidatorService,
+    VisualDiscoveryService,
+  ],
+  exports: [DiscoveryService, AtsResolverService, FreshnessValidatorService, VisualDiscoveryService],
 })
 export class DiscoveryModule {}
